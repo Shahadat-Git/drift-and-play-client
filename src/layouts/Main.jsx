28de4bc1bde384cffa9af27490a727116f64bd9f
Shahadat-Git <noyon560@gmail.com>
2023-06-07 18:51:43 +0600

    footer added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../shared/Header/Header';
+import Footer from '../shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
-            <h3>This is main layout</h3>
+            <div className='lg:min-h-[calc(100vh-559px)]'>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
